35828705324e94c27a59725481c2e4ddf110b962
Andrew Evans <78834502+AndrewEvansDev@users.noreply.github.com>
2021-05-26 03:41:00 -0700

    need to fix stuff stilll

--- server.py
+++ server.py
@@ -5,27 +5,44 @@ import json
 
 app = Flask(__name__)
 api_key = "1a62361f6606959256c2fcad3db52014"
+api_key2 = "7b30caac8f2413b21398febc12fb8d68"
 
 
 @app.route('/')
 @app.route('/home')
-def index():
+def home():
     return render_template("index.html")
+
 
 @app.route('/about')
 def about():
     return render_template("about.html")
 
+
 @app.route('/weather')
-def weather_page():
+def weather():
     return render_template("weather.html")
 
 @app.route('/api/weather', methods=["POST"])
-def weather():
+def get_weather():
     #get data from js request
     lat = request.json['lat']
     lon = request.json['lon']
     url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
+    
+    
+    response = requests.get(url) #call the open weather api
+    data = json.loads(response.text) #parse txt to json
+    return data #return weather as a json object
+
+@app.route('/api/locationdecoding', methods=["POST"])
+def decode_location():
+    #get data from js request
+
+    lat = request.json['lat']
+    lon = request.json['lon']
+    url = "http://api.positionstack.com/v1/reverse?access_key=%s&query=%s,%s" % (api_key2, lat, lon)
+
     
     
     response = requests.get(url) #call the open weather api
--- static/app.js
+++ static/app.js
@@ -22,19 +22,32 @@ function locationObtained(position) {
         success: function(res) {
             // console.log("Server says: ", res);
             displayWeather(res);
-            getLocal(position);
-            //display the weather information on HTML
+            displayForecast(res)
+                //display the weather information on HTML
         },
         error: function(details) {
             console.error("Error sending data", details);
         },
     });
+    $.ajax({
+        url: "/api/locationdecoding",
+        type: "POST",
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        success: function(res) {
+            console.log(res)
+            displayCity(res)
+        },
+        error: function(details) {
+            console.error('error', details)
+        }
+    })
 }
 
 displayWeather = (res) => {
     console.log(res);
     var showWeather = `
-    <div>
+    <div class="weather-wrap">
     <img src="http://openweathermap.org/img/wn/${res.current.weather[0].icon}@2x.png">
     <span class="weather-desc">${res.current.weather[0].description} </span>
     <span>${Math.round((res.current.temp * 9) / 5) + 32}&deg;<b>F</b></span>  
@@ -43,13 +56,26 @@ displayWeather = (res) => {
     document.getElementById("weather-data").innerHTML = showWeather;
 };
 
-function getLocal(position) {
-    console.log(position)
-        //google api to display locality to come
+displayForecast = (res) => {
+    for (var i = 0; i < 7; i++) {
+        console.log(i)
+        var li = document.createElement('li');
+        var ul = document.getElementById("forecast-wrap")
+        console.log(li)
+        li.innerHTML = `<span>${(res.daily[i].temp[0] * 9 / 5) + 32}${res.daily[i].weather[0].description}</span>`
+        console.log(li)
+        document.getElementById("forecast-wrap").appendChild(li);
+        ul.appendChild(li)
+    }
 }
 
+displayCity = (res) => {
+    var city = `${res.data[0].locality}, ${res.data[0].region}`;
+    document.querySelectorAll("span.city-location").innerText = city;
+}
+
+
 function init() {
-    console.log("Flask page");
     getLocation();
 }
 
